43c3c7a96ce54e12bfe8fe610258b6a7df0a47f4
Michael McFarland <michael.mcfarland@luckie.com>
2018-12-23 18:03:09 -0500

    Video Player Component Added and Styled

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <VideoPlayer/>
   </div>
 </template>
 
 <script>
+import VideoPlayer from './components/VideoPlayer'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    VideoPlayer
+  }
 }
 </script>
 
@@ -16,8 +20,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

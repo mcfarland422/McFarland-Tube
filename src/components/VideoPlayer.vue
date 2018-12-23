0544c46ce9b5c2b50f308aa63c8d56931d9127e7
Michael McFarland <michael.mcfarland@luckie.com>
2018-12-23 17:45:54 -0500

    Initial Setup

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="video-player">
+    Welcome to McFarlandTube
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoPlayer',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
